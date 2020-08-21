fdaa5078435a4968a5afa31980e85929050a7c07
Lucas Geitner <lucas.geitner@outlook.com>
2020-08-21 16:17:25 +0200

    add react-native native driver support

--- src/classes/Fuego.ts
+++ src/classes/Fuego.ts
@@ -1,4 +1,8 @@
 import * as firebase from 'firebase/app'
+import ReactNativeFirebase from '@react-native-firebase/app';
+import firestore from '@react-native-firebase/firestore';
+import auth from '@react-native-firebase/auth';
+import functions from '@react-native-firebase/functions';
 
 // import 'firebase/firestore'
 // import 'firebase/auth'
@@ -6,15 +10,37 @@ import * as firebase from 'firebase/app'
 
 type Config = Parameters<typeof firebase.initializeApp>[0]
 
+interface ConfixExtend extends Config {
+  useNativeDriver?: boolean
+}
+
+
 export class Fuego {
   public db: ReturnType<firebase.app.App['firestore']>
   public auth: typeof firebase.auth
   public functions: typeof firebase.functions
-  constructor(config: Config) {
-    this.db = !firebase.apps.length
+
+  constructor(config: ConfixExtend) {
+
+    if(config.useNativeDriver){
+      // @ts-ignore
+      const admin =  !ReactNativeFirebase.apps.length
+      ? ReactNativeFirebase.initializeApp(config)
+      : ReactNativeFirebase.app()
+
+      // @ts-ignore
+      this.db = firestore()
+      // @ts-ignore
+      this.auth = auth()
+      // @ts-ignore
+      this.functions = functions()
+
+    } else {
+      this.db = !firebase.apps.length
       ? firebase.initializeApp(config).firestore()
       : firebase.app().firestore()
-    this.auth = firebase.auth
-    this.functions = firebase.functions
+      this.auth = firebase.auth
+      this.functions = firebase.functions
+    }
   }
 }
